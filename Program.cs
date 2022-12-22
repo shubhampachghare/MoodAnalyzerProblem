f7efad71bdf167a0d7ba567a585f3e64c0227942
shubhampachghare <shubhampachghare@gmail.com>
2022-12-22 22:36:22 +0530

    [Shubham]-Welcome to mood analyzer problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyzerProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to mood analyzer problem");
+        }
+    }
+}
